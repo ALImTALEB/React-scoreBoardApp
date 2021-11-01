a8338eb1f89d8b0dafba216621d59a7febf76d52
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2021-11-02 00:44:57 +0100

    first commit

--- app.js
+++ app.js
@@ -1,105 +1,86 @@
-const Header = (props) => {
-  return (
-    <header>
-      <span className="stats">Players: {props.totalPlayers}</span> 
-      <h1>{ props.title }</h1>
-    </header>
-  );
-}
-
-class Counter extends React.Component {
-  
-  state = { 
-    score: 100 
-  };
-  
-  incrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score + 1 
-    }));
-  }
-  
-  decrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score - 1 
-    }));
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
-        <span className="counter-score"> {this.state.score} </span>
-        <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
-      </div>    
-    );
-  }
-}
-  
-const Player= (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
-
-      <Counter />
-    </div>
-  );
-}
-
-class App extends React.Component {
-  
-  state = {
-    players: [
-      {
-        name: "Guil",
-        id: 1,
+const players = [
+    {
+        name: "Ali",
+        score: 69,
+        id:1
       },
       {
-        name: "Treasure",
-        id: 2,    
+        name: "Baha",
+        score: 68,
+        id:2
       },
       {
-        name: "Ashley",
-        id: 3,    
+        name: "Ghayth",
+        score: 67,
+        id:3
       },
       {
-        name: "James",
-        id: 4,    
+        name: "Noussair",
+        score: 66,
+        id:4
+      },
+      {
+        name: "Hichem",
+        score: 65,
+        id:5
+      },
+      {
+        name: "Nader",
+        score: 64,
+        id:6
       }
-    ]
-  };
-        
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
-      return {
-        players: prevState.players.filter(p => p.id !== id)
-      }
-    });
-  }
-  
-  render() {
-    return (
-      <div className="scoreboard">
-        <Header 
-          title="My Scoreboard" 
-          totalPlayers={this.state.players.length} 
-        />
-        
-        {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
-            removePlayer={ this.handleRemovePlayer }      
-            name={player.name}
-            id={player.id}
-            key={player.id.toString()}
-          />
-        )}  
-      </div>
-    );
-  }
-}  
+]
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = (props) => { 
+    
+    return (
+        <header>
+            <h1>{props.title}</h1>
+            <span className="stats" >Players: {props.totalPlayers}</span>
+        </header>
+    )
+     }
+
+const Player = (props) => {
+   return (
+       <div className="player">
+           <span className="player-name">
+           {props.name}
+           </span>
+           <Counter score={props.score} />
+       </div>
+   )
+}
+
+const Counter = (props) => {
+    return (
+<div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <span className="counter-score">{props.score}</span>
+    <button className="counter-action increment" > + </button>
+</div>
+    )
+}
+
+const App = (props) => {
+    return (
+        <div className="scoreboard">
+            <Header
+             title="Pexa Scoreboard"
+              totalPlayers={props.initialPlayers.length} />
+            {/* players list*/}
+            {props.initialPlayers.map( player => 
+                <Player
+                 name={player.name}
+                 score={player.score}
+                 key={player.id.toString()}
+                  />
+            )}
+        </div>
+    )
+}
+
+ReactDOM.render(
+   <App initialPlayers={players} />,
+   document.getElementById('root')
+)
