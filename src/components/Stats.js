a2eea61dfbc48340899640de365c7fde807af45c
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2021-11-14 19:19:24 +0100

    using contextAPI

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,9 +5,9 @@ const Stats = () => {
 
     return (
       <Consumer>
-        { context => {
-          const totalPlayers = context.players.length
-    const totalPoints = context.players.reduce( (total, player) => {
+        { ({players}) => {
+          const totalPlayers = players.length
+    const totalPoints = players.reduce( (total, player) => {
         return total+player.score
     }, 0 )
           return (
